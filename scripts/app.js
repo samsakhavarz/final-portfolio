91a4b84788c4a5a7a6b2e281c5857cfc7b366d40
Sam <samsakhavarz@gmail.com>
2018-12-17 22:45:26 -0500

    additional style updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,25 @@
 $(function () {
 
-    $(".menu-link").click(function (e) {
+    $(".menu-link").on('click', function (e) {
         e.preventDefault();
         $(".menu").toggleClass("open");
         $(".menu-overlay").toggleClass("open");
     });
 
+    $(".nav-link").on('click', function() {
+        $(".menu").toggleClass("open");
+        $(".menu-overlay").toggleClass("open");
+
+    });
+
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+
+    
 });
+
